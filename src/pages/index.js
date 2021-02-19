e903eccb66435b1e4d5e50f7a67ac6d73116d0e4
Luiz Leite <luiz_particular@yahoo.com.br>
2021-02-18 23:44:45 -0300

    1st commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
+import Layout from '../components/Layout';
 
 export default function Home() {
-  return <div>YOUTUBE</div>;
+  return (
+    <Layout>
+      <div>YOUTUBE</div>
+      <Button color="primary" variant="contained">
+        Hello Word
+      </Button>
+    </Layout>
+  );
 }
